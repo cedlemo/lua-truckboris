ceb7ccfb2233017edcf7cccf9a65a349af3de809
cedlemo <cedlemo@gmx.com>
2015-11-03 18:47:18 +0100

    Add functions example and use it in README

--- test/functions.lua
+++ test/functions.lua
@@ -1,8 +1,3 @@
-# lua Truckboris
-
-lua bindings to the truckboris library. (https://github.com/cedlemo/truckboris)
-
-```lua
 local truckboris = require("truckboris")
 
 local parser = truckboris.HeaderParser()
@@ -33,33 +28,4 @@ for _,f in ipairs(functions) do
     print("\t\tParam : ".. p:type():name() .. "  " .. p:name())
   end
 end
-```
-
-# Build:
-
-## Standard:
-
-The autotools will detect automaticaly your lua version
-and install the library in a path build on the prefix variable.
-
-    ./autogen.sh --prefix=/usr
-    make
-    sudo make install
-
-The library files will be isntalled in /usr/lib/lua/LUA_VERSION/trucboris.
-
-## Custom directory:
-
-You can still use the *libdir* variable. If your current version of lua
-is 5.3 for example:
-
-    ./autogen.sh --libdir=/usr/lib/lua/5.3
-    make
-    sudo make install
-
-
-# Clean:
-
-    sudo make uninstall
-    make maintainer clean
 
